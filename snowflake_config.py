18fdd774849e132139d1f719c8a165b59fd7c0cb
Fernando <fernandobasaez@gmail.com>
2025-06-02 11:53:37 +1000

    Create snowflake_config.py

---
+++ snowflake_config.py
@@ -0,0 +1,8 @@
+# snowflake_config.py
+SNOWFLAKE_CONFIG = {
+    'account': 'XMAEPLH-TL62578',     # Reemplaza con tu account identifier
+    'user': 'FERNANDOBASAEZ',                  # Reemplaza con tu usuario
+    'password': 'Colaemono%$_23658',           # Reemplaza con tu contraseña
+    'warehouse': 'COMPUTE_WH',             # Warehouse por defecto
+    'role': 'ACCOUNTADMIN'                 # Role por defecto
+}
